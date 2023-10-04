d17abeec491c3e24ea1e1b621c4450c13625127a
Johnmarc22 <johnmarcconsolacion3@gmail.com>
2023-10-04 09:49:45 +0800

    feat: added html and js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let firstFavNum = 20;
+let secondFavNum = 30;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum**2);
